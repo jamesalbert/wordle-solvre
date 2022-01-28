305060b6fd4740bae1a3b15ac07939b57c3ab1a3
jamesalbert <jalbert@roblox.com>
2022-01-28 12:19:09 -0800

    go headless

--- cheat.py
+++ cheat.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.powerlanguage.co.uk/wordle/")
 game_app = driver.find_element_by_tag_name("game-app")
 answer = driver.execute_script("return arguments[0].solution", game_app)
